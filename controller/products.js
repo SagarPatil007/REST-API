3f74058e0f2834fe1f10c9fc1b7c94c05261ce02
sagar patil <sagarpatil@gmail.com>
2022-12-23 11:57:21 +0530

    Adding Sorting functionality

--- controller/products.js
+++ controller/products.js
@@ -1,12 +1,35 @@
 const Product = require("../models/products");
 
 const getAllProducts  = async(req,res) => {
-    const myData = await Product.find(req.query);
+
+    const {compnay,name,featured,sort} = req.query;
+    const queryObject = {};
+
+    if(compnay){
+        queryObject.compnay = compnay;
+    }
+
+    if(featured){
+        queryObject.featured = featured;
+    }
+
+    if(name){
+        queryObject.name = {$regex:name,$options:"i"};
+    }
+
+    let apiData = Product.find(queryObject);
+
+    if(sort){
+        let sortFix = sort.replace(","," ");
+        apiData = apiData.sort(sortFix);
+    }
+
+    const myData = await apiData;
     res.status(200).json({myData});
 };
 
 const getAllProductsTesting  = async(req,res) => {
-    const myData = await Product.find(req.query);
+    const myData = await apiData;
     res.status(200).json({myData});
 }
 
